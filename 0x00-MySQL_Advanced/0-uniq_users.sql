b0cacbeb1e048d30f0e3a65024ef557e4ed8afb2
Ermi24et <lilermi24@gmail.com>
2024-03-20 16:39:17 +0300

    we are all unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- a script to create a table users
+-- following requirements
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
