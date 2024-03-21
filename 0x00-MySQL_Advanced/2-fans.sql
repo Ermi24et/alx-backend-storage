337874e33ccf3f5990c221e3c3f14cb1e92b79cc
Ermi24et <lilermi24@gmail.com>
2024-03-21 15:24:22 +0300

    best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,6 @@
 -- sql script that ranks country origins of bands
 -- ordered by the number of non-unique fans
-SELECT origin, fans FROM metal_bands Ascendant;
+SELECT origin, COUNT(*) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
