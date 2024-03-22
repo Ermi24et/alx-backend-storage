458bbde44644afa69a1d04c91fcbd39668cc5c6f
Ermi24et <lilermi24@gmail.com>
2024-03-22 03:05:41 +0300

    average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser that
 -- computes and store the average score for a student.
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN usr_id INTEGER)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN usr_id INT)
 BEIGN
         SET @avrg = (SELECT AVG(score) AS avrg FROM corrections WHERE user_id = usr_id);
         UPDATE users SET average_score = @avrg WHERE id = usr_id;
